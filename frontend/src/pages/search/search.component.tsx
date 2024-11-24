43c8e2aa2baf3c483dff16c211ddaa20abe13bd7
Javier <jcmsr07@gmail.com>
2024-11-24 01:33:01 +0100

    Add search input

--- frontend/src/pages/search/search.component.tsx
+++ frontend/src/pages/search/search.component.tsx
@@ -1,6 +1,20 @@
-import { Button } from '@mui/material';
-import * as React from 'react';
+import { AppBar, Box, Button, TextField, Toolbar, Typography } from '@mui/material';
 
-export const Search = () => { 
-    return <Button variant="contained">Hello world</Button>;
+export const Search = () => {
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+            <AppBar position="static">
+                <Toolbar sx={{ display: 'flex' }}>
+                    <Typography variant="h6" noWrap component="div">Web Scrapping App</Typography>
+                </Toolbar>
+            </AppBar>
+            <Box sx={{ flex: '1' }}>
+                <TextField id="outlined-basic" label="Search" variant="outlined" />
+                <Button variant="contained">Search</Button>
+            </Box>
+        </Box>
+    )
 };
+
+// TODO: componetizar search + button (sacar a otro fichero e importarlo aqui)
+// añadir estilos para centrar y poner bonito el buscador
