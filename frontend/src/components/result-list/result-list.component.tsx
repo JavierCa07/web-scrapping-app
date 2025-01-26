e887e53ec123ad8885b1518ece337dc884d5a994
Javier <jcmsr07@gmail.com>
2025-01-26 11:33:49 +0100

    Improve UI

--- frontend/src/components/result-list/result-list.component.tsx
+++ frontend/src/components/result-list/result-list.component.tsx
@@ -2,9 +2,9 @@ import { Box } from "@mui/material"
 import { useState } from "react";
 
 const SIMILARITY_RANGES = {
-  "lightgreen": [0, 50],
-  "coral": [50, 80],
-  "tomato": [80, 100]
+  "#adf3ad": [0, 50],
+  "#eca55e": [50, 80],
+  "#ee7964": [80, 100]
 };
 
 const getSimilarityColor = (percentage: number) => {
@@ -25,19 +25,20 @@ export const ResultList = ({ results }) => {
     const getHeightByResultIndex = (index: number) => expandedResultIndexes.includes(index) ? "auto" : "120px";
 
     return results.map((result, i) => {
-      return <Box key={i} sx={{ display: "flex", border: "1px solid black", width: "clamp(600px, 50%, 800px)", margin: "10px", height: getHeightByResultIndex(i), overflowY: "hidden"}}>
+      return <Box key={i} sx={{ display: "flex", border: "1px solid grey", width: "clamp(600px, 50%, 800px)", margin: "10px", height: getHeightByResultIndex(i), overflowY: "hidden"}}>
         <Box sx={{ flex: 1, color: "black", display: "flex", justifyContent: "center", alignItems: "center", cursor: "pointer", fontSize: "2rem" }} onClick={() => handleExpandClick(i)}>
           +
         </Box>
         <Box sx={{ display: "flex", flexDirection: "column", flex: 16 }}>
-          <Box sx={{ flex: 1, background: "deepskyblue" }}>
+          <Box sx={{ flex: 1, background: "#98beca", minHeight: "24px", overflow: "hidden", paddingLeft: "10px" }}>
             <a href={result.source} target="_blank" rel="noopener noreferrer">{result.source}</a>
           </Box>
           <Box sx={{ flex: 10 }}>
           {result.text}
           </Box>
         </Box>
-        <Box sx={{ display: "flex", flex: 2, alignItems: "center", justifyContent: "center", fontSize: "2rem", background: getSimilarityColor(result.percentage) }}>
+        <Box sx={{ display: "flex", flexDirection: "column", flex: 2, alignItems: "center", justifyContent: "center", fontSize: "2rem", background: getSimilarityColor(result.percentage) }}>
+          <div style={{ color: "#222222", fontSize: "8px"}}>similarity</div>
           {result.percentage}
         </Box>
       </Box>
